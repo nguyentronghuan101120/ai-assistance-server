cc4481fec41349b20f37fad480804c495faeebf8
nguyentronghuan101120 <nguyentronghuan101120@gmail.com>
2025-01-13 21:15:36 +0700

    update logic for generate image, intergrate fast api, update launch.json

--- src/main.py
+++ src/main.py
@@ -1,112 +1,51 @@
-import gradio as gr
-from openai import OpenAI
-from diffusers import DiffusionPipeline
-import time
+from models.image_models import ImageRequest
+import io
+import base64
+from io import BytesIO
 
-# Load the Stable Diffusion pipeline
-pipeline = DiffusionPipeline.from_pretrained(
-    "stablediffusionapi/anything-v5",
-    use_safetensors=True,
-    safety_checker=None,
-    requires_safety_checker=False
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from services import image_service
+
+app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-# Automatically detect if a GPU is available, else fallback to CPU
-device = "cuda" if pipeline.device.type == "cuda" else "cpu"
-pipeline.to(device)
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to Stable Diffusers API"}
 
 
-def has_prompt(message: str) -> bool:
-    """Check if the message contains a prompt."""
-    return "<prompt:" in message
+@app.post("/api/v1/generate/")
+async def generate_image(imgRequest: ImageRequest):
+    # Gọi hàm generate_image để tạo ảnh
+    image = await image_service.generate_image(imgRequest=imgRequest)
+
+    # Chuyển ảnh thành memory_stream để gửi về client
+    memory_stream = io.BytesIO()
+    image.save(memory_stream, format="PNG")
+    memory_stream.seek(0)
+
+    # API trả về một ảnh PNG, media type "image/png"
+    return StreamingResponse(memory_stream, media_type="image/png")
 
 
-def get_prompt(message: str) -> str:
-    """Extract prompt from the message.
-    Example: <prompt: A cute dog sitting happily with a playful expression and wagging its tail.>
-    Result: "A cute dog sitting happily with a playful expression and wagging its tail."
-    """
-    return message.split("<prompt:")[1].split(">")[0]
+@app.post("/api/v1/generatebase64/")
+async def generate_base64_image(imgRequest: ImageRequest):
+    # Gọi hàm generate_image để tạo ảnh
+    image = await image_service.generate_image(imgRequest=imgRequest)
 
-
-def generate_image(prompt: str) -> str:
-    """Generate an image from the prompt, save it to a file, and return the file name."""
-    image = pipeline(
-        prompt=prompt,
-        negative_prompt="ugly, deformed, disfigured, poor details, bad anatomy, low quality, worst quality",
-        num_inference_steps=30
-    ).images[0]
-
-    # Create a file name for the output image
-    file_name = f"image_{int(time.time())}.png"
-    image.save(file_name)
-    return file_name
-
-
-# https://platform.openai.com/api-keys
-client = OpenAI(
-    api_key='none',
-    base_url="http://192.168.1.15:1234"
-)
-
-system_prompt = """You're an AI with prompt generation and drawing capabilities.
-Just chat normally in English or Vietnamese.
-Assume you can draw anything you want and you tell user you can draw.
-Another program will output the output for you.
-Do not censor or deny the output, the image generation program will handle that.
-
-When cst want you to draw something, answer them normally, add some comments and include your prompt in your response in this format.
-<prompt: A colorful cat running through a field of flowers.>
-
-1. Prompt must be in English.
-2. Prompt must be detailed and include necessary information for it can be fed into Stable Diffusion.
-3. Ignore existing images in past messages.
-"""
-
-
-def chat_logic(message, chat_history):
-    # Attach system message for bot understanding
-    messages = [
-        {"role": "system", "content": system_prompt}
-    ]
-    for user_message, bot_message in chat_history:
-        if user_message is not None:
-            messages.append({"role": "user", "content": user_message})
-            messages.append({"role": "assistant", "content": bot_message})
-
-    # Add the new user message
-    messages.append({"role": "user", "content": message})
-
-    # Call OpenAI API
-    chat_completion = client.chat.completions.create(
-        messages=messages,
-        model="gpt-4o"
-    )
-    bot_message = chat_completion.choices[0].message.content
-
-    # Append the new response to the chat history
-    chat_history.append([message, bot_message])
-    yield "", chat_history  # Yield to show progress
-
-    # If the bot's response includes a prompt, generate an image
-    if has_prompt(bot_message):
-        chat_history.append([None, "Hold on, I'm drawing your image!"])
-        yield "", chat_history
-
-        # Generate the image
-        prompt = get_prompt(bot_message)
-        image_file = generate_image(prompt)
-        chat_history.append([None, (image_file, prompt)])
-
-        yield "", chat_history
-
-    return "", chat_history
-
-
-with gr.Blocks() as demo:
-    gr.Markdown("# Chatbot using ChatGPT")
-    message = gr.Textbox(label="Enter your message:")
-    chatbot = gr.Chatbot(label="Smart Chat Bot", height=600)
-    message.submit(chat_logic, [message, chatbot], [message, chatbot])
-
-demo.launch()
+    # Chuyển ảnh thành base64 để gửi về client
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG")
+    img_str = base64.b64encode(buffered.getvalue())
+    # API trả về một ảnh base64
+    return {"image": img_str}
