8f85371af77bf5405919890a066cf8d4673fd723
LeoNguyen <nguyentronghuan101120@gmail.com>
2025-02-16 23:28:23 +0700

    update image generation logic and model ID

--- src/utils/image_pipeline.py
+++ src/utils/image_pipeline.py
@@ -1,5 +1,5 @@
 import torch
-from diffusers import AutoPipelineForText2Image, DPMSolverMultistepScheduler
+from diffusers import StableDiffusionPipeline
 
 # Device setup
 _device = (
@@ -8,25 +8,31 @@ _device = (
     else "cpu"
 )
 torch.backends.cuda.matmul.allow_tf32 = True  # Enable TF32 for performance on CUDA
-_model_id = "stable-diffusion-v1-5/stable-diffusion-v1-5"
+# _model_id = "stable-diffusion-v1-5/stable-diffusion-v1-5"
+_model_id_or_link = "https://huggingface.co/quakumei/REALISM_BY_STABLE_YOGI/blob/main/realismBYSTABLEYOGI_v4LCM.safetensors"
 
 
 # Load the pipeline lazily
 _pipeline = None
 
-def get_pipeline() -> AutoPipelineForText2Image:
+def get_pipeline() -> StableDiffusionPipeline:
     global _pipeline
     if _pipeline is None:
         try:
-            _pipeline = AutoPipelineForText2Image.from_pretrained(
-                _model_id,
+            # _pipeline = StableDiffusionPipeline.from_pretrained(
+            #     _model_id_or_link,
+            #     torch_dtype=torch.bfloat16,
+            #     variant="fp16",
+            #     # safety_checker=True,
+            #     use_safetensors=True,
+            # )
+            _pipeline = StableDiffusionPipeline.from_single_file(
+                _model_id_or_link,
                 torch_dtype=torch.bfloat16,
                 variant="fp16",
                 # safety_checker=True,
                 use_safetensors=True,
             )
-            _pipeline.scheduler = DPMSolverMultistepScheduler.from_config(_pipeline.scheduler.config)
-            # _pipeline.enable_model_cpu_offload()
             _pipeline.to(_device)
         except Exception as e:
             raise RuntimeError(f"Failed to load the model: {e}")
