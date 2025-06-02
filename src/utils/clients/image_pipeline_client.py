d2849b9a4316b1168368d611afbe4ab44ace896e
LeoNguyen <nguyentronghuan101120>
2025-06-02 16:58:52 +0700

    Refactor image pipeline client and update requirements: Move torch and StableDiffusionPipeline imports inside the load_pipeline function for better error handling. Add ImportError exception to guide users on missing dependencies. Comment out the torch version in requirements_for_server.txt for clarity.

--- src/utils/clients/image_pipeline_client.py
+++ src/utils/clients/image_pipeline_client.py
@@ -1,17 +1,26 @@
-import torch
-from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion import (
-    StableDiffusionPipeline,
-)
 from constants.config import IMAGE_MODEL_ID_OR_LINK, TORCH_DEVICE
 from utils.timing import measure_time
 
-torch.backends.cuda.matmul.allow_tf32 = True  # Enable TF32 for performance on CUDA
 
 pipeline = None
 
 
 def load_pipeline():
     global pipeline
+    try:
+        import torch
+        from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion import (
+            StableDiffusionPipeline,
+        )
+
+        torch.backends.cuda.matmul.allow_tf32 = (
+            True  # Enable TF32 for performance on CUDA
+        )
+    except ImportError:
+        raise ImportError(
+            "diffusers is not installed. Please install it using 'pip install diffusers'."
+        )
+
     with measure_time("Load image pipeline"):
         pipeline = StableDiffusionPipeline.from_pretrained(
             IMAGE_MODEL_ID_OR_LINK,
